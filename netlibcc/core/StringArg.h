e9deea22e85528b7635ece2cb8b1823b1778b336
kohakus <iszhangyt@163.com>
2020-07-28 17:04:04 +0800

    add StringArg ctor(s)

--- netlibcc/core/StringArg.h
+++ netlibcc/core/StringArg.h
@@ -10,6 +10,7 @@ public:
     // ctors
     StringArg(const char* str) : str_(str) {}
     StringArg(const std::string& str) : str_(str.c_str()) {}
+    StringArg(const std::string&& str) : str_(str.c_str()) {}
 
     const char* c_str() const {
         return str_;
